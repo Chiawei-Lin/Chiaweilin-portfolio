8b5728a66d855ba84f6924f6daa6b682851685be
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-02-02 14:00:19 +0300

    translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'clp-root',
-  imports: [RouterOutlet],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'chiaweilin-portfolio';
+  constructor (public translate: TranslateService) {
+    this.translate.addLangs(['zh', 'en']);
+    this.translate.setDefaultLang('zh');
+    this.translate.use('zh');
+  }
 }
