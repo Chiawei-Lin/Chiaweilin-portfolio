5a7d3d7d5b42d516f03a4659a6da05c50e43bd90
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-02-02 14:30:53 +0300

    layout

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clp-nav',
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+
+}
