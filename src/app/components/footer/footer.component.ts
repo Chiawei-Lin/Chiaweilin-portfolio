508cb201e1cfedc185e3758c4ad7d9b208edb375
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-02-03 15:59:16 +0300

    footer modifications

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { AbstractComponent } from '../../models/abstract-component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'clp-footer',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent extends AbstractComponent {
 
+  constructor () {
+    super({
+      translationPrefix: 'FOOTER'
+    });
+  }
+
+  toLink (url: string) {
+    window.open(url, "_blank");
+  }
 }
