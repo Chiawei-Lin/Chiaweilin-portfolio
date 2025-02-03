14e803a6b46f185b9c0b5310c8dd7e9387e242f2
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-02-03 19:34:47 +0300

    Auto-generated commit

--- chunk-YNRSVZDE.js
+++
@@ -1,1 +0,0 @@
-import{Ha as o,Va as n,Wa as c,gb as p}from"./chunk-2YHNQMH5.js";var r=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["clp-project"]],decls:2,vars:0,template:function(e,i){e&1&&(n(0,"p"),p(1,"project works!"),c())},encapsulation:2})};export{r as ProjectComponent};
