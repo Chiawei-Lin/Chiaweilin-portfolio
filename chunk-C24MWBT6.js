4177bec6547c30f6e010fd6250982d7ffa382672
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-02-15 14:27:20 +0300

    Auto-generated commit

--- chunk-C24MWBT6.js
+++
@@ -1,1 +0,0 @@
-import{Sa as o,eb as n,fb as a,rb as p}from"./chunk-SGFH4T6O.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["clp-about"]],decls:2,vars:0,template:function(t,m){t&1&&(n(0,"p"),p(1,"about works!"),a())},encapsulation:2})};export{s as AboutComponent};
