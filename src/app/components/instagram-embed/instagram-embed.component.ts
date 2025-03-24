6beeca24551cc64853c8d151910c9c28feb398c5
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-03-24 18:13:52 +0300

    feat: added instagram embedding implementation

--- src/app/components/instagram-embed/instagram-embed.component.ts
+++ src/app/components/instagram-embed/instagram-embed.component.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, SecurityContext, SimpleChanges } from '@angular/core';
 import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -8,16 +7,26 @@ import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
   templateUrl: './instagram-embed.component.html',
   styleUrl: './instagram-embed.component.scss'
 })
-export class InstagramEmbedComponent implements OnChanges {
+export class InstagramEmbedComponent implements OnChanges, AfterViewInit {
   @Input() url?: string;
-  sanitizedUrl?: SafeResourceUrl;
 
-  constructor (private _sanitizer: DomSanitizer, private httpClient: HttpClient) { }
+  ngAfterViewInit (): void {
+    // @ts-ignore
+    if (window['instgrm']) {
+      instgrm.Embeds.process();
+    }
+  }
 
 
   ngOnChanges (changes: SimpleChanges): void {
     if (changes['url']?.currentValue) {
-      this.sanitizedUrl = this._sanitizer.bypassSecurityTrustResourceUrl(this.url!);
+      "https://www.instagram.com/reel/Cn1pbY3LULe/?utm_source=ig_embed&amp;utm_campaign=loading"
     }
+  }
+}
+
+declare namespace instgrm {
+  namespace Embeds {
+    function process (): void;
   }
 }
